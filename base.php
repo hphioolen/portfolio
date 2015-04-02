05fcfd71ed2009370c8126970352a3fe7dfc5a1d
hphioolen <hphioolen@imac-b7ca20.mecooffset.local>
2015-04-02 17:29:34 +0200

    header less

--- base.php
+++ base.php
@@ -32,7 +32,7 @@ use Roots\Sage\Wrapper;
     </div><!-- /.wrap -->
     
     <?php if (is_front_page()): ?>	
-		<?php get_template_part('templates/includes/portfolio-categorie'); ?>template
+		<?php get_template_part('templates/includes/portfolio-categorie'); ?>
 	<?php endif; ?> 
     
     <?php
